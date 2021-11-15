2ba3a9b2ec6739b43ced6df023ddcae0741ca51b
emirerdogdu <emirerdogdu@gmail.com>
2021-11-16 00:44:46 +0300

    template created

---
+++ src/ConsoleAppTemplate/ConsoleAppTemplate/App.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+
+public class App
+{
+    private readonly IConfiguration _config;
+
+    public App(IConfiguration config)
+    {
+        _config = config;
+    }
+
+    public void Run()
+    {
+        Console.WriteLine($"App is running ({_config.GetValue<string>("Version")})");
+    }
+}
---
+++ src/ConsoleAppTemplate/ConsoleAppTemplate/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+var services = ConfigureServices();
+var serviceProvider = services.BuildServiceProvider();
+
+serviceProvider.GetService<App>()?.Run();
+
+static IServiceCollection ConfigureServices()
+{
+    var services = new ServiceCollection();
+
+    var config = LoadConfiguration();
+    services.AddSingleton(config);
+
+    /* add services here*/
+
+    services.AddTransient<App>();
+
+    return services;
+}
+
+static IConfiguration LoadConfiguration()
+{
+    var builder = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+    return builder.Build();
+}
